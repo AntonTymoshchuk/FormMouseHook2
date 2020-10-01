7ff34e51937f98d2dc11b9e7ce534dab2d810dfa
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:51:09 +0300

    Add project files.

---
+++ FormMouseHook2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormMouseHook2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            RamGecTools.MouseHook MouseHook = new RamGecTools.MouseHook();
+            MouseHook.Install();
+            MouseHook.LeftButtonDown += MouseHook_LeftButtonDown;
+        }
+
+        private void MouseHook_LeftButtonDown(RamGecTools.MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            Random random = new Random();
+            this.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+    }
+}
